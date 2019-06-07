910b28cb9686cbde4f3b4354d04aba55d0525bb3
Leianne Louis <leilouis112@gmail.com>
2019-06-06 20:47:58 -0400

    completed ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -9,6 +9,21 @@
 int main(void)
 {
     // Your code here 
-    
+    FILE * fp;
+
+    fp = fopen ("text.txt", "w+");
+    fprintf(fp, "%s", "working");
+    int rc = fork();
+    if(rc < 0) {
+        printf("Fork Error");
+    } else if(rc == 0) {
+        fprintf(fp, "%s", "I am writing from the child process!\n");
+    } else {
+        fprintf(fp, "%s", "I am writing from the Parent process!\n");
+    }
+    fclose(fp);
     return 0;
 }
+
+
+//Both the parent and child have access to the file that is created... The text added by the child process is added before the code added to the parent process!
