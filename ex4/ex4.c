133c2c8d5f8be9f0103c33e2f18b43c0608222ef
Leianne Louis <leilouis112@gmail.com>
2019-06-07 00:29:30 -0400

    completed ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -11,6 +11,14 @@
 int main(void)
 {
     // Your code here    
-
+    int rc = fork();
+    if(rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        execl("/bin/ls", "ls", "-la", NULL);
+    } else {
+        printf("Done");
+    }
     return 0;
 }
